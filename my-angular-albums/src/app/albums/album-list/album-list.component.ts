2926f63227792658540f46be44d98c2ac2e48ad2
itdev2016 <lmzhen95@gmail.com>
2019-09-26 13:25:07 -0400

    Ch8 Ex3

--- my-angular-albums/src/app/albums/album-list/album-list.component.ts
+++ my-angular-albums/src/app/albums/album-list/album-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Album } from "../album.model";
 import { ALBUMS } from "../albums.data";
 import { AlbumService } from '../shared/album.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: "app-album-list",
@@ -9,7 +10,7 @@ import { AlbumService } from '../shared/album.service';
   styleUrls: ["./album-list.component.css"]
 })
 export class AlbumListComponent implements OnInit {
-  albumsArray: Album[];
+  albumsArray: Observable<Album[]>;
   clickedAlbum: string;
 
   constructor(private albumService: AlbumService) { }
@@ -23,9 +24,6 @@ export class AlbumListComponent implements OnInit {
   }
 
   getAlbums() {
-    this.albumService.getAlbums()
-    .subscribe(
-        albums => this.albumsArray = albums,
-        error => console.log("Error: ", error));
+    this.albumsArray = this.albumService.getAlbums();
   }
 }
